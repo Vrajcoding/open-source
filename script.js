a85963f00ae7973a0e70f6beb74f3aa14f56247f
Vrajcoding <vraj11@gmail.com>
2026-03-16 22:12:23 +0530

    added some synchronize

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("this is another commit")
+
+setImmediate(() => {
+    console.log("immediate function")
+})
+
+process.nextTick(() => {
+    console.log("nextTick function")
+})
+
+setTimeout(() => {
+    console.log("setTimeout function")
+}, 0)
+
+
+Promise.resolve().then(() => {
+    console.log("this is promise function");
+})
+
+console.log("hello")
